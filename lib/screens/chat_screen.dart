7de4dac09bbf658bb5b93c77f7094219f317a380
GauravXd <grvdhingra1999@gmail.com>
2020-04-07 06:03:10 +0530

    Revesed listview and inverted list, notifies

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -130,15 +130,31 @@ class MesageBubble extends StatelessWidget {
             '$sender',
             style: Theme.of(context).textTheme.body2,
           ),
-          Material(
-            borderRadius: BorderRadius.circular(10),
-            elevation: 5,
-            color: Colors.lightBlueAccent,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-              child: Text(
-                '$text',
-                style: Theme.of(context).textTheme.body2,
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Material(
+              borderRadius: isMe
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    )
+                  : BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+              elevation: 5,
+              color: isMe ? Colors.lightBlueAccent : Colors.white,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 15),
+                child: Text(
+                  '$text',
+                  style: isMe
+                      ? TextStyle(color: Colors.white)
+                      : TextStyle(color: Colors.black),
+                ),
               ),
             ),
           ),
@@ -166,7 +182,6 @@ class MessagesStream extends StatelessWidget {
           for (var message in messages) {
             final messageText = message.data['text'];
             final messageSender = message.data['sender'];
-
             final currentUser = loggedInUser.email;
 
             final messageBubble = MesageBubble(
